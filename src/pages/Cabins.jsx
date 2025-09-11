c3d753c83267fc00cc210183801586e411f15670
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-11 18:00:36 +0200

    feat: add create cabin form with react-hook-form and API integration

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import CabinTable from '../features/cabins/CabinTable';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
 
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
+import Button from '../ui/Button';
 
 function Cabins() {
+  const [showForm, setShowForm] = useState();
+
   return (
     <>
       <Row type="horizontal">
@@ -12,6 +17,11 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((prev) => !prev)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
