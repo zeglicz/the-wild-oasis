7fdf94553d5e5e9066082b4182e819633af867a5
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-12 10:28:26 +0200

    feat: add error handling and validation to CreateCabinForm - extract FormRow and adjust AppLayout layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,7 +5,8 @@ import styled from 'styled-components';
 
 const StyledAppLayout = styled.div`
   display: grid;
-  grid-template-columns: 26rem 1fr;
+  /* grid-template-columns: 26rem 1fr; */
+  grid-template-columns: 13rem 1fr;
   grid-template-rows: auto 1fr;
   height: 100vh;
 `;
@@ -13,6 +14,7 @@ const StyledAppLayout = styled.div`
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+  overflow: auto;
 `;
 
 const Container = styled.div`
