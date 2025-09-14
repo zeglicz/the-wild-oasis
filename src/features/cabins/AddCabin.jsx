657c2b94b381a734548ee0536eb12c282dfff42b
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-14 11:48:42 +0200

    feat: refactor Modal into context-based compound component - add Modal.Open and Modal.Window for flexible usage

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,24 +1,43 @@
-import { useState } from 'react';
-
 import CreateCabinForm from './CreateCabinForm';
+import CabinTable from './CabinTable';
 import Button from '../../ui/Button';
 import Modal from '../../ui/Modal';
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((prev) => !prev)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(close)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(close)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open open="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open open="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((prev) => !prev)}>
+//         Add new cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(close)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(close)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -2,6 +2,7 @@
 import { createPortal } from 'react-dom';
 import styled from 'styled-components';
 import { HiXMark } from 'react-icons/hi2';
+import { cloneElement, createContext, useContext, useState } from 'react';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -52,18 +53,46 @@ const Button = styled.button`
   }
 `;
 
-function Modal({ children, onClose }) {
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState('');
+
+  const close = () => setOpenName('');
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, open: openWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(openWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={onClose}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
-        <div>{children}</div>
+
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body,
   );
 }
+
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
