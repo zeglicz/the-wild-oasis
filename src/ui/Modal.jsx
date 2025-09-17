47e63bc71c12c30757493c48f4d7aadbc4a68560
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-17 19:32:51 +0200

    feat: add useOutsideClick hook and integrate with Modal.Window for closing on outside click

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -3,6 +3,7 @@ import { createPortal } from 'react-dom';
 import styled from 'styled-components';
 import { HiXMark } from 'react-icons/hi2';
 import { cloneElement, createContext, useContext, useState } from 'react';
+import { useOutsideClick } from '../hooks/useOutsideClick';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -76,11 +77,13 @@ function Open({ children, open: openWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
