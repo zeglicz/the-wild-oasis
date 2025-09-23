57d47718144766a4514b5d44c2065f5924a72e5e
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-23 11:39:27 +0200

    feat: add dark mode support - create DarkModeContext with provider and custom hook for toggling theme - implement DarkModeToggle button with sun/moon icons - update GlobalStyles with light and dark theme variables - update App to wrap content with DarkModeProvider - update HeaderMenu to include DarkModeToggle - update Logo to switch between light and dark variants - fix apiAuth updateCurrentUser with await supabase.auth.updateUser

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { useDarkMode } from '../context/DarkModeContext';
 
 const StyledLogo = styled.div`
   text-align: center;
@@ -10,11 +11,11 @@ const Img = styled.img`
 `;
 
 function Logo() {
-  return (
-    <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
-    </StyledLogo>
-  );
+  const { isDarkMode } = useDarkMode();
+
+  const src = isDarkMode ? '/logo-dark.png' : '/logo-light.png';
+
+  return <StyledLogo>{<Img src={src} alt="Logo" />}</StyledLogo>;
 }
 
 export default Logo;
